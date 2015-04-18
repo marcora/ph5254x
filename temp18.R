80d2ac58421cf9399a7553aed2c30e9913d8ab38
Edoardo Marcora <edoardo.marcora@gmail.com>
2015-04-17 23:03:17 -0700

    Renamed some script files

--- temp18.R
+++ temp18.R
@@ -1,54 +1,106 @@
 # library(devtools)
-# install_github("genomicsclass/ERBS")
-library(ERBS)
+# install_github("genomicsclass/GSE5859Subset")
 
-data(HepG2)
-data(GM12878)
+library(GSE5859Subset)
+data(GSE5859Subset)
+dim(geneExpression)
+dim(sampleInfo)
+dim(geneAnnotation)
 
-HepG2 # HepG2 cell line
-GM12878 # GM12878 cell line
+identical(colnames(geneExpression), sampleInfo$filename)
+identical(rownames(geneExpression), geneAnnotation$PROBEID)
 
-# overlaps
-fo = findOverlaps(HepG2, GM12878)
-erbs = HepG2[queryHits(fo)]
-erbs = granges(erbs)
+pd = AnnotatedDataFrame(sampleInfo)
 
-# intersection
-erbs2 = intersect(HepG2, GM12878)
-erbs2 = granges(erbs2)
+rownames(pd) = colnames(geneExpression)
 
-# sort
-erbs = sort(erbs)
-erbs2 = sort(erbs2)
+pData(pd)["GSM136530.CEL.gz","date"]
 
-# confirm same chr and strand
-all(seqnames(erbs) == seqnames(erbs2))
-all(strand(erbs) == strand(erbs2))
+varLabels(pd)[1]
 
-# proportion of identical ranges based on start and end
-mean(start(erbs) == start(erbs2) & end(erbs2) == end(erbs3))
 
-# the intersection should be smaller
-all(width(erbs2) <= width(erbs))
+fd = AnnotatedDataFrame(geneAnnotation)
+
+rownames(fd) = rownames(geneExpression)
+
+pData(fd)["204810_s_at","CHR"]
+
+eset = ExpressionSet(assayData = geneExpression, phenoData = pd, featureData = fd)
+
+annotation(eset) = "hgfocus"
+
+dim(pData(eset))
+dim(exprs(eset))
+dim(featureData(eset))
+
+ind1 = which(featureData(eset)$CHR=="chrY")
+ind2 =  pData(eset)$group==1
+femaleY = colMeans(exprs(eset)[ind1, ind2]) 
+maleY = colMeans(exprs(eset)[ind1, !ind2]) 
+boxplot(maleY,femaleY)
+median(maleY)-median(femaleY)
+
+
+eset[1:10,1:5]
 
 ###############################################################################
+
 library(Homo.sapiens)
+genes = genes(Homo.sapiens)
 
-ghs = genes(Homo.sapiens)
+library(hgfocus.db) # microarray platform used to obtain the data
 
-tss = resize(ghs, 1)
+# get the ENTREZIDs associated with the PROBEIDs for this array
+map = select(hgfocus.db, keys=featureNames(eset), columns="ENTREZID", keytype="PROBEID")
 
-start(tss["100113402"])
+# since we obtain a  multiple map, pick the first (match does this automatically)
+index1 = match(featureNames(eset), map[,1])
 
-fo = findOverlaps(HepG2, GM12878)
-erbs = HepG2[queryHits(fo)]
+# now use this to map to the genes GRanges
+index2 = match(map[index1,2], as.character(mcols(genes)$GENEID))
 
-index = nearest(erbs[4], tss)
+# remove NAs
+index3 = which(!is.na(index2))
+index2 = index2[index3]
 
-geneid = names(tss[index])
-geneid
+# subset objects to map
+genes = genes[index2,]  
+neweset =  eset[index3,]
 
-keytypes(Homo.sapiens)
-columns(Homo.sapiens)
+# create a summarized experiment for eset
+se = SummarizedExperiment(assays=exprs(neweset),
+                          rowData=genes,
+                          colData=DataFrame(pData(neweset)))
+se
 
-select(Homo.sapiens, keytype="GENEID", keys=geneid, columns = c("SYMBOL"))
+dim(assay(se))
+length(granges(se))
+
+tss = resize(granges(se), 1)
+
+head(tss)
+
+sum(start(tss) <= 50*10^6 & seqnames(tss) == "chr1")
+
+# plot differential expression across the genome
+
+# re-order se
+se = se[order(granges(se)),]
+ind = se$group==1
+de = rowMeans(assay(se)[,ind]) - rowMeans(assay(se)[,!ind])
+chrs = unique(seqnames(se))
+
+# chr1-4
+par(mfrow=c(3,2))
+for(i in c(1:4)){
+  ind = which(seqnames(se) == chrs[i])
+  plot(start(se)[ind], de[ind], ylim=c(-1,1), main=as.character(chrs[i]))
+  abline(h=0)
+}
+
+# chrX and Y
+for(i in 23:24){
+  ind = which(seqnames(se) == chrs[i])
+  plot(start(se)[ind], de[ind], ylim=c(-5,5), main=as.character(chrs[i]))
+  abline(h=0)
+}
--- temp9.R
+++ temp9.R
@@ -1,106 +1,54 @@
 # library(devtools)
-# install_github("genomicsclass/GSE5859Subset")
+# install_github("genomicsclass/ERBS")
+library(ERBS)
 
-library(GSE5859Subset)
-data(GSE5859Subset)
-dim(geneExpression)
-dim(sampleInfo)
-dim(geneAnnotation)
+data(HepG2)
+data(GM12878)
 
-identical(colnames(geneExpression), sampleInfo$filename)
-identical(rownames(geneExpression), geneAnnotation$PROBEID)
+HepG2 # HepG2 cell line
+GM12878 # GM12878 cell line
 
-pd = AnnotatedDataFrame(sampleInfo)
+# overlaps
+fo = findOverlaps(HepG2, GM12878)
+erbs = HepG2[queryHits(fo)]
+erbs = granges(erbs)
 
-rownames(pd) = colnames(geneExpression)
+# intersection
+erbs2 = intersect(HepG2, GM12878)
+erbs2 = granges(erbs2)
 
-pData(pd)["GSM136530.CEL.gz","date"]
+# sort
+erbs = sort(erbs)
+erbs2 = sort(erbs2)
 
-varLabels(pd)[1]
+# confirm same chr and strand
+all(seqnames(erbs) == seqnames(erbs2))
+all(strand(erbs) == strand(erbs2))
 
+# proportion of identical ranges based on start and end
+mean(start(erbs) == start(erbs2) & end(erbs2) == end(erbs3))
 
-fd = AnnotatedDataFrame(geneAnnotation)
-
-rownames(fd) = rownames(geneExpression)
-
-pData(fd)["204810_s_at","CHR"]
-
-eset = ExpressionSet(assayData = geneExpression, phenoData = pd, featureData = fd)
-
-annotation(eset) = "hgfocus"
-
-dim(pData(eset))
-dim(exprs(eset))
-dim(featureData(eset))
-
-ind1 = which(featureData(eset)$CHR=="chrY")
-ind2 =  pData(eset)$group==1
-femaleY = colMeans(exprs(eset)[ind1, ind2]) 
-maleY = colMeans(exprs(eset)[ind1, !ind2]) 
-boxplot(maleY,femaleY)
-median(maleY)-median(femaleY)
-
-
-eset[1:10,1:5]
+# the intersection should be smaller
+all(width(erbs2) <= width(erbs))
 
 ###############################################################################
-
 library(Homo.sapiens)
-genes = genes(Homo.sapiens)
 
-library(hgfocus.db) # microarray platform used to obtain the data
+ghs = genes(Homo.sapiens)
 
-# get the ENTREZIDs associated with the PROBEIDs for this array
-map = select(hgfocus.db, keys=featureNames(eset), columns="ENTREZID", keytype="PROBEID")
+tss = resize(ghs, 1)
 
-# since we obtain a  multiple map, pick the first (match does this automatically)
-index1 = match(featureNames(eset), map[,1])
+start(tss["100113402"])
 
-# now use this to map to the genes GRanges
-index2 = match(map[index1,2], as.character(mcols(genes)$GENEID))
+fo = findOverlaps(HepG2, GM12878)
+erbs = HepG2[queryHits(fo)]
 
-# remove NAs
-index3 = which(!is.na(index2))
-index2 = index2[index3]
+index = nearest(erbs[4], tss)
 
-# subset objects to map
-genes = genes[index2,]  
-neweset =  eset[index3,]
+geneid = names(tss[index])
+geneid
 
-# create a summarized experiment for eset
-se = SummarizedExperiment(assays=exprs(neweset),
-                          rowData=genes,
-                          colData=DataFrame(pData(neweset)))
-se
+keytypes(Homo.sapiens)
+columns(Homo.sapiens)
 
-dim(assay(se))
-length(granges(se))
-
-tss = resize(granges(se), 1)
-
-head(tss)
-
-sum(start(tss) <= 50*10^6 & seqnames(tss) == "chr1")
-
-# plot differential expression across the genome
-
-# re-order se
-se = se[order(granges(se)),]
-ind = se$group==1
-de = rowMeans(assay(se)[,ind]) - rowMeans(assay(se)[,!ind])
-chrs = unique(seqnames(se))
-
-# chr1-4
-par(mfrow=c(3,2))
-for(i in c(1:4)){
-  ind = which(seqnames(se) == chrs[i])
-  plot(start(se)[ind], de[ind], ylim=c(-1,1), main=as.character(chrs[i]))
-  abline(h=0)
-}
-
-# chrX and Y
-for(i in 23:24){
-  ind = which(seqnames(se) == chrs[i])
-  plot(start(se)[ind], de[ind], ylim=c(-5,5), main=as.character(chrs[i]))
-  abline(h=0)
-}
+select(Homo.sapiens, keytype="GENEID", keys=geneid, columns = c("SYMBOL"))
